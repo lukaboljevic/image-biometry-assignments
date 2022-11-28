c4528851e9ea7da54b601ef75bb6c827197ef2b1
lukaboljevic <lukaboljevic.boljevic@gmail.com>
2022-11-29 00:41:54 +0100

    Even more annotations

--- Assignment3/errors.py
+++ Assignment3/errors.py
@@ -16,16 +16,19 @@ column_types = {
 def get_errors():
     df = pd.read_csv("./annotations.csv", sep=";", dtype=column_types)
     df = df[df["Err"].isna() == False]
-    return df[["CID", "Image"]].values
+    return df[["CID", "Image", "Err"]].values
 
 
 def main(error_list):
-    for dir_name, img_name in error_list:
+    print("#############################")
+    print(f"# Number of errors: {len(error_list)}")
+    print("#############################\n")
+    for dir_name, img_name, error in error_list:
         image = cv.cvtColor(cv.imread(f"images/{dir_name}/{img_name[1:]}.png"), cv.COLOR_BGR2RGB)
         mask = cv.imread(f"masks/{dir_name}/{img_name[1:]}.png", cv.IMREAD_GRAYSCALE)
         alpha = 0.4*(mask > 0)
         plt.figure(figsize=(9, 7))
-        plt.title(f"Subject: {dir_name}, image: {img_name[1:]}")
+        plt.title(f"Subject: {dir_name}, image: {img_name[1:]}, error: {error}")
         plt.imshow(image)
         plt.imshow(mask, alpha = alpha)
         plt.show()
